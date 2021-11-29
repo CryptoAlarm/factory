020cbc5445045000eeab7e7befcf19b6b7a80f25
H9Solutions <paulo.artlab@gmail.com>
2021-11-28 22:52:23 -0300

    Server struct set and get

--- src/providers/server/getTokensList.ts
+++ src/providers/server/getTokensList.ts
@@ -4,7 +4,14 @@ import "dotenv/config"
 import serverAPI from "./api"
 
 
+interface TokensProps{
+  
+  contract: string
+  ref: string
+  network: string
+  isCoingecko: boolean
+}
 export const getTokensList = () => {
 
-  serverAPI.get("/getPrice")
+  serverAPI.get<TokensProps>("/getPrice")
 }
