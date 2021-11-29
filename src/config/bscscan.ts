732fda50b362e27899ae9f44112de1dfaaadaea4
H9Solutions <paulo.artlab@gmail.com>
2021-11-29 08:52:13 -0300

    abi contract, bscscan contract, apeswap contract

---
+++ src/config/bscscan.ts
@@ -0,0 +1,3 @@
+export const BscscanEndpoint = "https://bsc-dataseed1.binance.org"
+export const BNBTokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c".toLowerCase();
+export const USDTokenAddress = "0xe9e7cea3dedca5984780bafc599bd69add087d56".toLowerCase(); //USDT
