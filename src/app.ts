bfefbcddb45bba3e2f9a82472b87cd46a62b0bdf
H9Solutions <paulo.artlab@gmail.com>
2021-11-29 08:55:05 -0300

    debug

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import {coinGeckoPrice} from "./providers/tokens/coingecko"
 import {PancakeswapPrice} from "./providers/tokens/pancake"
 import {FactoryChain} from "./providers/tokens/factoryChain"
 import {Mir4} from "./providers/tokens/mir4"
+import {ApeSwapWeb3Client} from "./providers/tokens/apeswap"
 
 import { TokenData } from "./types";
 
@@ -11,6 +12,13 @@ let TokenPricesList = {} as TokenData
 
 ;(async() => {
 
+
+  const Token = new ApeSwapWeb3Client({
+    network: "bscscan",
+    refreshBnbInSeconds: 15
+  })
+
+  console.log(await Token.getPriceBUSDBased("0xcf2D2CE89AeD0073540C497fcF894Ea22d37C7aF"))
 
 
   console.log(await Mir4({
