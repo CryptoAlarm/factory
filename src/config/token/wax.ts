0755a09891ef41f7f1aa09ceec66c9c1a21b9e04
H9Solutions <paulo.artlab@gmail.com>
2022-02-19 19:56:06 -0300

    config alchor

--- src/config/token/wax.ts
+++ src/config/token/wax.ts
@@ -1,1 +1,62 @@
 export const waxEndPoint = (id: string | number) => `https://wax.alcor.exchange/api/markets/${id}/charts?resolution=240&`
+export const AlchorExchangeSetup = {
+  ESB: {
+    id: 159,  
+    value: 0,
+    ref: "esbcontracts"
+  },
+  FWW: {
+    id: 104,
+    value: 0,
+    ref: "FWW"
+  },
+  FWG: {
+    id: 106,
+    value: 0,
+    ref: "FWG"
+    
+  },
+  FWF: {
+    id: 105,
+    value: 0,
+    ref: "FWF",    
+  },
+  GMM: {
+    id: 339,
+    value: 0,
+    ref: "GMM",  
+  },
+  GME: {
+    id: 337,
+    value: 0,
+    ref: "GME",  
+  },
+  GMF: {
+    id: 338,
+    value: 0,
+    ref: "GMF",  
+  },
+  GMD: {
+    id: 340,
+    value: 0,
+    ref: "GMD",  
+  }
+} as AlchorExchangeSetup
+
+export interface ApiResponse {
+  _id: string
+  volume: number
+  open: number
+  high:number
+  low: number
+  close: number
+  time: number
+}
+export type AlchorExchangeInfo ={
+  id: number,
+  value: number
+  ref: string
+}
+export interface AlchorExchangeSetup {
+  [key: string]: AlchorExchangeInfo
+}
